07eb500678feda9622b93816e8497ca4fc30e45c
Arthur Artugue <artuguearthur@gmail.com>
2024-10-21 16:42:12 +0800

    polished add,edit,view task

--- lib/pages/task/add_task.dart
+++ lib/pages/task/add_task.dart
@@ -54,13 +54,13 @@ class _AddTaskPageState extends State<AddTaskPage> {
               ),
             ),
             colorScheme: Theme.of(context).colorScheme.copyWith(
-                  // Title, selected date and day selection background (dark and light mode)
-                  surface: DeckColors.backgroundColor,
-                  primary: DeckColors.primaryColor,
-                  // Title, selected date and month/year picker color (dark and light mode)
-                  onSurface: DeckColors.white,
-                  onPrimary: DeckColors.white,
-                ),
+              // Title, selected date and day selection background (dark and light mode)
+              surface: DeckColors.backgroundColor,
+              primary: DeckColors.primaryColor,
+              // Title, selected date and month/year picker color (dark and light mode)
+              onSurface: DeckColors.white,
+              onPrimary: DeckColors.white,
+            ),
             textButtonTheme: TextButtonThemeData(
               style: TextButton.styleFrom(
                 textStyle: const TextStyle(
@@ -95,126 +95,197 @@ class _AddTaskPageState extends State<AddTaskPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const DeckBar(
-        title: 'Add New Task',
-        color: DeckColors.white,
-        fontSize: 24,
-      ),
+      // appBar: const DeckBar(
+      //   title: 'Add New Task',
+      //   color: DeckColors.white,
+      //   fontSize: 24,
+      // ),
       body: SafeArea(
-        top: true,
-        bottom: false,
-        left: true,
-        right: true,
-        minimum: const EdgeInsets.only(left: 20, right: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Divider(
-                color: DeckColors.white,
-                thickness: 2,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: BuildTextBox(
-                  hintText: "Enter Task Title",
-                  showPassword: false,
-                  controller: _titleController,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: BuildTextBox(
-                  hintText: "Enter Deadline",
-                  onTap: () => _selectDate(
-                      context), // Pass context to _selectDate method
-                  controller: _dateController,
-                  isReadOnly: true,
-                  rightIcon: Icons.calendar_today_outlined,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: BuildTextBox(
-                  hintText: "Enter Task Description",
-                  showPassword: false,
-                  controller: _descriptionController,
-                  isMultiLine: true,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: BuildButton(
-                    buttonText: "Save",
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    radius: 10,
-                    backgroundColor: DeckColors.primaryColor,
-                    textColor: DeckColors.white,
-                    size: 16,
-                    fontSize: 16,
-                    borderWidth: 0,
-                    borderColor: Colors.transparent,
-                    onPressed: () async {
-                      ///loading dialog
-                      showLoad(context);
+          top: true,
+          bottom: false,
+          left: true,
+          right: true,
+          minimum: const EdgeInsets.only(left: 20, right: 20),
+          child: SingleChildScrollView(
+            child: Column(
+                children: [
+                  const Image(
+                    image: AssetImage('assets/images/AddDeck_Header.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  // const Divider(
+                  //   color: DeckColors.white,
+                  //   thickness: 2,
+                  // ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(padding: EdgeInsets.only(top: 10),
+                          child:
+                          Text('Add a New task',
+                            style: TextStyle(
+                              fontFamily: 'Fraiche',
+                              color: DeckColors.primaryColor,
+                              fontSize: 52,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20,bottom: 10),
+                          child: Text(
+                            'Title',
+                            style: TextStyle(
+                              fontFamily: 'Nunito',
+                              color: DeckColors.primaryColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ),
+                        BuildTextBox(
+                          controller: _titleController,
+                          hintText: "Enter Task Title",
+                        ),
 
-                      if(_dateController.text.isEmpty || _titleController.text.isEmpty || _descriptionController.text.isEmpty){
-                        /// stop loading
-                        hideLoad(context);
-                        ///display error
-                        showInformationDialog(context, "Error adding task","A Text field is blank! Please fill all the text fields and try again.");
-                        return;
-                      }
+                        Padding(
+                            padding: EdgeInsets.only(top: 20,bottom: 10),
+                            child:
+                            Text(
+                              'Due Date',
+                              style: TextStyle(
+                                fontFamily: 'Nunito',
+                                color: DeckColors.primaryColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            )
+                        ),
+                        BuildTextBox(
+                          hintText: "Enter Due Date",
+                          onTap: () => _selectDate(context), // Pass context to _selectDate method
+                          controller: _dateController,
+                          isReadOnly: true,
+                          rightIcon: Icons.calendar_today_outlined,
+                        ),
+                        const Padding(
+                            padding: EdgeInsets.only(top: 20,bottom: 10),
+                            child:
+                            Text(
+                              'Description',
+                              style: TextStyle(
+                                fontFamily: 'Nunito',
+                                color: DeckColors.primaryColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            )
+                        ),
+                        BuildTextBox(
+                          hintText: "Enter Task Description",
+                          showPassword: false,
+                          controller: _descriptionController,
+                          isMultiLine: true,
+                        ),
+                        const Padding(
+                            padding: EdgeInsets.only(top: 20,bottom: 10),
+                            child:
+                            Text(
+                              'Priority',
+                              style: TextStyle(
+                                fontFamily: 'Nunito',
+                                color: DeckColors.primaryColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            )
+                        ),
+                        RadioButtonGroup(
+                          buttonLabels: ['High', 'Medium', 'Low'],
+                          buttonColors: [Colors.red, Colors.yellow, Colors.blue],
+                          isClickable: true,
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(top: 20),
+                            child:
+                            BuildButton(
+                                buttonText: "Save",
+                                height: 50,
+                                width: MediaQuery.of(context).size.width,
+                                radius: 10,
+                                backgroundColor: DeckColors.primaryColor,
+                                textColor: DeckColors.white,
+                                size: 16,
+                                fontSize: 16,
+                                borderWidth: 2,
+                                borderColor: DeckColors.white,
+                                onPressed: () async {
+                                  ///loading dialog
+                                  showLoad(context);
+                                  if(_dateController.text.isEmpty || _titleController.text.isEmpty || _descriptionController.text.isEmpty){
+                                    /// stop loading
+                                    hideLoad(context);
+                                    ///display error
+                                    showInformationDialog(context, "Error adding task","A Text field is blank! Please fill all the text fields and try again.");
+                                    return;
+                                  }
+                                  print(DateTime.parse(_dateController.text));
+                                  print(DateTime.now());
+                                  if(DateTime.parse(_dateController.text).add(const Duration(hours: 23, minutes: 59, seconds: 59)).isBefore(DateTime.now())){
+                                    /// stop loading
+                                    hideLoad(context);
+                                    ///display error
+                                    showInformationDialog(context, "Error adding task"," Past deadlines aren't allowed. Please try again.");
 
-                      print(DateTime.parse(_dateController.text));
-                      print(DateTime.now());
-                      if(DateTime.parse(_dateController.text).add(const Duration(hours: 23, minutes: 59, seconds: 59)).isBefore(DateTime.now())){
-                        /// stop loading
-                        hideLoad(context);
-                        ///display error
-                        showInformationDialog(context, "Error adding task"," Past deadlines aren't allowed. Please try again.");
+                                    return;
+                                  }
+                                  Map<String, dynamic> data = {
+                                    "user_id": AuthService().getCurrentUser()?.uid,
+                                    "title": _titleController.text,
+                                    "description" : _descriptionController.text,
+                                    "set_date": DateTime.now(),
+                                    "end_date": DateTime.parse(_dateController.text).add(const Duration(hours: 23, minutes: 59, seconds: 59)),
+                                    "is_done": false,
+                                    "is_deleted": false,
+                                    "done_date": DateTime.now(),
+                                  };
+                                  /// stop loading
+                                  hideLoad(context);
+                                  Provider.of<TaskProvider>(context, listen: false).addTask(data);
+                                  Navigator.pop(context);
+                                }
+                            ),
 
-                        return;
-                      }
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(top: 20),
+                            child:
+                            BuildButton(
+                              buttonText: "Cancel",
+                              height: 50,
+                              width: MediaQuery.of(context).size.width,
+                              radius: 10,
+                              backgroundColor: DeckColors.white,
+                              textColor: DeckColors.primaryColor,
+                              size: 16,
+                              fontSize: 16,
+                              borderWidth: 0,
+                              borderColor: Colors.transparent,
+                              onPressed: () {
+                                print("Cancel button clicked");
+                                Navigator.pop(context);
+                              },
+                            ),
 
-                      Map<String, dynamic> data = {
-                        "user_id": AuthService().getCurrentUser()?.uid,
-                        "title": _titleController.text,
-                        "description" : _descriptionController.text,
-                        "set_date": DateTime.now(),
-                        "end_date": DateTime.parse(_dateController.text).add(const Duration(hours: 23, minutes: 59, seconds: 59)),
-                        "is_done": false,
-                        "is_deleted": false,
-                        "done_date": DateTime.now(),
-                      };
-                      /// stop loading
-                      hideLoad(context);
-                      Provider.of<TaskProvider>(context, listen: false).addTask(data);
-                      Navigator.pop(context);
-                    }),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: BuildButton(
-                  buttonText: "Cancel",
-                  height: 50,
-                  width: MediaQuery.of(context).size.width,
-                  radius: 10,
-                  backgroundColor: DeckColors.white,
-                  textColor: DeckColors.primaryColor,
-                  size: 16,
-                  fontSize: 16,
-                  borderWidth: 0,
-                  borderColor: Colors.transparent,
-                  onPressed: () {
-                    print("Cancel button clicked");
-                    Navigator.pop(context);
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]
+            ),
+          )
       ),
     );
   }
