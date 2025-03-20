5d5070c9709bddf426bc2695d3477718f9c409ab
faithelle1311 <2022-102980@rtu.edu.ph>
2025-03-21 07:25:16 +0800

    added popscope in report a problem page

--- lib/pages/settings/support and policies/report_a_problem.dart
+++ lib/pages/settings/support and policies/report_a_problem.dart
@@ -9,6 +9,7 @@ import '../../misc/colors.dart';
 import '../../misc/custom widgets/appbar/auth_bar.dart';
 import '../../misc/custom widgets/buttons/custom_buttons.dart';
 import '../../misc/custom widgets/buttons/radio_button.dart';
+import '../../misc/custom widgets/dialogs/confirmation_dialog.dart';
 import '../../misc/widget_method.dart';
 
 
@@ -34,171 +35,204 @@ class _ReportAProblemState extends State<ReportAProblem> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const AuthBar(
-        automaticallyImplyLeading: true,
-        title: 'Report A Problem',
-        color: DeckColors.primaryColor,
-        fontSize: 24,
-      ),
-      backgroundColor: DeckColors.backgroundColor,
-      body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 15.0, right: 15.0),
-                    child: Text('Report A Problem',
-                      style: TextStyle(
-                        fontFamily: 'Fraiche',
-                        fontWeight: FontWeight.bold,
-                        color: DeckColors.primaryColor,
-                        fontSize: 40,
-                        height: 1.1,
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 20.0),
-                    child: Text('Found something on Deck that doesn’t '
-                        'seem right? Help us improve Deck by reporting it.',
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                        fontFamily: 'Nunito-Regular',
-                        color: DeckColors.primaryColor,
-                        fontSize: 16,
-                        height: 1.3,
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 20.0),
-                    child: Text('What issue are you reporting?',
-                      style: TextStyle(
-                        fontFamily: 'Fraiche',
-                        color: DeckColors.primaryColor,
-                        fontSize: 32,
-                        height: 1.1,
-                      ),
-                    ),
-                  ),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (bool didpop) async {
+        if (didpop) {
+          return;
+        }
 
-            ///this handles the radio button
-            Padding(
-              padding: const EdgeInsets.only(right: 15.0),
-              child: BuildRadioButton(
-                numberOfButtons: 4, //number of radio buttons
-                //radio button label
-                buttonLabels: const [
-                  'Someone\'s Deck Content',
-                  'AI-Generated Content',
-                  'Bug Issues',
-                  'Something Else',
-                ],
-                //subtexts for the radio button label
-                buttonSubtexts: const [
-                  'Inappropriate, offensive, or misleading content in public decks.',
-                  'Harmful, incorrect, or biased AI-generated quiz or flashcard content.',
-                  'App crashes, broken features, or unexpected behavior.',
-                  '', //null because there is no subtext for the 4th option
-                ],
-                textStyle:
-                const TextStyle(
-                    fontFamily: 'Fraiche',
-                    color: DeckColors.primaryColor,
-                    fontSize: 24),
-                subtextStyle:
-                const TextStyle(
-                    fontFamily: 'Nunito-Regular',
-                    color: DeckColors.primaryColor,
-                    fontSize: 16),
-                    activeColor: DeckColors.primaryColor, //active radio button color
-                    inactiveColor: DeckColors.primaryColor, //inactive radio button color
-                    onButtonSelected: radioButtonSelected,//for radio buttons behavior
-              ),
-            ),
-                  ///shows the content based on the selected radio
-                  if (selectedRadio == 0) SomeoneDeckContent(), //Option 1: Someone's deck content
-                  if (selectedRadio == 1) AIGeneratedContent(), //Option 2: AI-Generated contet
-                  if (selectedRadio == 2) BugIssues(), //Option 3: Bug Issues
-                  if (selectedRadio == 3) SomethingElse(), //Option 4: Something Else
+        // Show the dialog only if a radio button is selected
+        if (selectedRadio != -1) {
+          final shouldPop = await showDialog<bool>(
+            context: context,
+            builder: (BuildContext context) {
+              return ShowConfirmationDialog(
+                title: 'Cancel report?',
+                text: 'If you go back now, all unsaved progress will be lost. ',
+                onConfirm: () {
+                  Navigator.of(context).pop();
+                },
+                onCancel: () {},
+              );
+            },
+          );
 
-                  ///--- E N D -----
-
-                  ///this section informs the user about the consequences or actions that will occur upon submitting the form.
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-                    child: Wrap(
-                      alignment: WrapAlignment.start,
-                      children: [
-                        const Text('By submitting this form, you agree to Deck'
-                            ' processing your information as outlined in our',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(
-                            fontFamily: 'Nunito-Regular',
-                            color: DeckColors.primaryColor,
-                            fontSize: 16,
-                            height: 1.3,
-                          ),
+          //If the user confirmed, pop the current route
+          if (shouldPop != null && shouldPop) {
+            Navigator.of(context).pop(true);
+          }
+        } else {
+          //No unsaved changes, allow pop without confirmation
+          Navigator.of(context).pop(true);
+        }
+      },
+      child: Scaffold(
+        appBar: const AuthBar(
+          automaticallyImplyLeading: true,
+          title: 'Report A Problem',
+          color: DeckColors.primaryColor,
+          fontSize: 24,
+        ),
+        backgroundColor: DeckColors.backgroundColor,
+        body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                      child: Text('Report A Problem',
+                        style: TextStyle(
+                          fontFamily: 'Fraiche',
+                          fontWeight: FontWeight.bold,
+                          color: DeckColors.primaryColor,
+                          fontSize: 40,
+                          height: 1.1,
                         ),
-                        ///this handles when user clicks the privacy policy
-                        InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              RouteGenerator.createRoute(const PrivacyPolicyPage()),
-                            );
-                          },
-                          borderRadius: BorderRadius.circular(8),
-                          splashColor: DeckColors.primaryColor.withOpacity(0.5),
-                          child: const Padding(
-                            padding: EdgeInsets.all(4.0),
-                            child: Text(
-                              'Privacy Policy',
-                              style: TextStyle(
-                                fontFamily: 'Nunito-ExtraBold',
-                                color: DeckColors.primaryColor,
-                                fontSize: 16,
-                                decoration: TextDecoration.underline,
-                                decorationColor: DeckColors.primaryColor,
-                                decorationThickness: 3,
-                              ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 20.0),
+                      child: Text('Found something on Deck that doesn’t '
+                          'seem right? Help us improve Deck by reporting it.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontFamily: 'Nunito-Regular',
+                          color: DeckColors.primaryColor,
+                          fontSize: 16,
+                          height: 1.3,
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 20.0),
+                      child: Text('What issue are you reporting?',
+                        style: TextStyle(
+                          fontFamily: 'Fraiche',
+                          color: DeckColors.primaryColor,
+                          fontSize: 32,
+                          height: 1.1,
+                        ),
+                      ),
+                    ),
+
+              ///this handles the radio button
+              Padding(
+                padding: const EdgeInsets.only(right: 15.0),
+                child: BuildRadioButton(
+                  numberOfButtons: 4, //number of radio buttons
+                  //radio button label
+                  buttonLabels: const [
+                    'Someone\'s Deck Content',
+                    'AI-Generated Content',
+                    'Bug Issues',
+                    'Something Else',
+                  ],
+                  //subtexts for the radio button label
+                  buttonSubtexts: const [
+                    'Inappropriate, offensive, or misleading content in public decks.',
+                    'Harmful, incorrect, or biased AI-generated quiz or flashcard content.',
+                    'App crashes, broken features, or unexpected behavior.',
+                    '', //null because there is no subtext for the 4th option
+                  ],
+                  textStyle:
+                  const TextStyle(
+                      fontFamily: 'Fraiche',
+                      color: DeckColors.primaryColor,
+                      fontSize: 24),
+                  subtextStyle:
+                  const TextStyle(
+                      fontFamily: 'Nunito-Regular',
+                      color: DeckColors.primaryColor,
+                      fontSize: 16),
+                      activeColor: DeckColors.primaryColor, //active radio button color
+                      inactiveColor: DeckColors.primaryColor, //inactive radio button color
+                      onButtonSelected: radioButtonSelected,//for radio buttons behavior
+                ),
+              ),
+                    ///shows the content based on the selected radio
+                    if (selectedRadio == 0) SomeoneDeckContent(), //Option 1: Someone's deck content
+                    if (selectedRadio == 1) AIGeneratedContent(), //Option 2: AI-Generated contet
+                    if (selectedRadio == 2) BugIssues(), //Option 3: Bug Issues
+                    if (selectedRadio == 3) SomethingElse(), //Option 4: Something Else
+
+                    ///--- E N D -----
+
+                    ///this section informs the user about the consequences or actions that will occur upon submitting the form.
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                      child: Wrap(
+                        alignment: WrapAlignment.start,
+                        children: [
+                          const Text('By submitting this form, you agree to Deck'
+                              ' processing your information as outlined in our',
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              fontFamily: 'Nunito-Regular',
+                              color: DeckColors.primaryColor,
+                              fontSize: 16,
+                              height: 1.3,
                             ),
                           ),
-                        )
-                      ],
+                          ///this handles when user clicks the privacy policy
+                          InkWell(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                RouteGenerator.createRoute(const PrivacyPolicyPage()),
+                              );
+                            },
+                            borderRadius: BorderRadius.circular(8),
+                            splashColor: DeckColors.primaryColor.withOpacity(0.5),
+                            child: const Padding(
+                              padding: EdgeInsets.all(4.0),
+                              child: Text(
+                                'Privacy Policy',
+                                style: TextStyle(
+                                  fontFamily: 'Nunito-ExtraBold',
+                                  color: DeckColors.primaryColor,
+                                  fontSize: 16,
+                                  decoration: TextDecoration.underline,
+                                  decorationColor: DeckColors.primaryColor,
+                                  decorationThickness: 3,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  ///------ E N D ---------
+                    ///------ E N D ---------
 
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0, right: 15.0, top:10, bottom: 20),
-                    child: BuildButton(
-                      onPressed: () {
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15.0, right: 15.0, top:10, bottom: 20),
+                      child: BuildButton(
+                        onPressed: () {
 
-                      },
-                      buttonText: 'Submit',
-                      height: 50.0,
+                        },
+                        buttonText: 'Submit',
+                        height: 50.0,
+                        width: MediaQuery.of(context).size.width,
+                        backgroundColor: DeckColors.primaryColor,
+                        textColor: DeckColors.white,
+                        radius: 10.0,
+                        borderColor: DeckColors.primaryColor,
+                        fontSize: 16,
+                        borderWidth: 0,
+                      ),
+                    ),
+
+                    Image.asset(
+                      'assets/images/Deck-Bottom-Image.png',
+                      fit: BoxFit.fitWidth,
                       width: MediaQuery.of(context).size.width,
-                      backgroundColor: DeckColors.primaryColor,
-                      textColor: DeckColors.white,
-                      radius: 10.0,
-                      borderColor: DeckColors.primaryColor,
-                      fontSize: 16,
-                      borderWidth: 0,
                     ),
-                  ),
-
-                  Image.asset(
-                    'assets/images/Deck-Bottom-Image.png',
-                    fit: BoxFit.fitWidth,
-                    width: MediaQuery.of(context).size.width,
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
+      ),
     );
   }
 }
