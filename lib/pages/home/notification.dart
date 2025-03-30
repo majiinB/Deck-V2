98ba8abdf70a4ae7746f0200cf86cb2e21709cff
lei-mon <leilamontemayor37@gmail.com>
2025-03-30 20:01:13 +0800

    added an app bar for back button

--- lib/pages/home/notification.dart
+++ lib/pages/home/notification.dart
@@ -4,6 +4,8 @@ import 'package:deck/pages/misc/colors.dart';
 import 'package:deck/pages/misc/deck_icons.dart';
 import 'package:flutter/material.dart';
 
+import '../misc/custom widgets/appbar/auth_bar.dart';
+import '../misc/custom widgets/appbar/deck_bar.dart';
 import '../misc/custom widgets/buttons/custom_buttons.dart';
 import '../misc/custom widgets/buttons/icon_button.dart';
 import '../misc/custom widgets/dialogs/alert_dialog.dart';
@@ -17,8 +19,21 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: DeckColors.backgroundColor,
-    );
+    return
+      SafeArea(
+          child: Scaffold(
+            backgroundColor: DeckColors.backgroundColor,
+
+            appBar: AuthBar(
+              automaticallyImplyLeading: true,
+              title: 'Notifications',
+              color: DeckColors.primaryColor,
+              fontSize: 24,
+              onRightIconPressed: () {
+
+              },
+            ),
+          )
+      );
   }
 }
