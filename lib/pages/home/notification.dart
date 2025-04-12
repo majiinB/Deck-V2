c5870f6fe7425c618a81eb645affa111c01390f6
lei-mon <leilamontemayor37@gmail.com>
2025-04-13 02:55:51 +0800

    add an if collection empy condition

--- lib/pages/home/notification.dart
+++ lib/pages/home/notification.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:deck/pages/misc/colors.dart';
 import 'package:deck/pages/misc/deck_icons.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,7 @@ import '../misc/custom widgets/buttons/custom_buttons.dart';
 import '../misc/custom widgets/buttons/icon_button.dart';
 import '../misc/custom widgets/dialogs/alert_dialog.dart';
 import '../misc/custom widgets/dialogs/confirmation_dialog.dart';
+import '../misc/custom widgets/functions/if_collection_empty.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
@@ -17,6 +19,8 @@ class NotificationPage extends StatefulWidget {
   _NotificationPageState createState() => _NotificationPageState();
 }
 class _NotificationPageState extends State<NotificationPage> {
+  final bool hasNotification = false;
+
   @override
   Widget build(BuildContext context) {
     return
@@ -30,9 +34,22 @@ class _NotificationPageState extends State<NotificationPage> {
               color: DeckColors.primaryColor,
               fontSize: 24,
               onRightIconPressed: () {
-
               },
             ),
+            body: hasNotification ? ListView.builder(itemBuilder: (context, count){
+
+            }) :
+            Center(
+              heightFactor:MediaQuery.of(context).size.height,
+              widthFactor: MediaQuery.of(context).size.width,
+              child: IfCollectionEmpty(
+                ifCollectionEmptyText: 'No Results Found',
+                ifCollectionEmptySubText:
+                'Try adjusting your search to \nfind what your looking for.',
+                ifCollectionEmptyHeight:
+                MediaQuery.of(context).size.height,
+              ),
+            )
           )
       );
   }
